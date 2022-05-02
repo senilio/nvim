d4327a8850ce0370cfad46be7f0a787a88627113
senilio <git@melt.se>
2022-05-02 12:04:27 +0200

    setup system clipboard shortcuts and disable default integration

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,3 +5,8 @@ expr_options = { expr = true, silent = true }
 --Remap space as leader key
 map({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 vim.g.mapleader = " "
+
+--Custom mappings
+map("v", "Y", '"*y', { silent = false }) -- YANK visual block to system clipboard
+map("n", "YY", '"*yy', { silent = false }) -- YANK line to system clipboard
+map("v", "P", '"*p', { silent = false }) -- Paste from system clipboard
