0aa6a23425120d266aab604a6aeebc9be72ecb7e
senilio <git@melt.se>
2023-09-14 08:29:45 +0200

    Fix nvim-tree error deprecation

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -161,12 +161,6 @@ require("nvim-tree").setup({
 		side = "left",
 		-- if true the tree will resize itself after opening a file
 		--		auto_resize = false,
-		mappings = {
-			-- custom only false will merge the list with the default mappings
-			-- if true, it will only use your list to set the mappings
-			custom_only = true,
-			-- list of mappings to set on the tree manually
-		},
 	},
 })
 
