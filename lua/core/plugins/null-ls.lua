033e68f9668fa39064d00f98b05dfac80d39a69f
senilio <emil@melt.se>
2023-10-08 22:24:55 +0200

    Updated null ls

--- lua/core/plugins/null-ls.lua
+++ lua/core/plugins/null-ls.lua
@@ -5,24 +5,43 @@ local M = {
   },
   config = function()
     local nls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
     nls.setup({
       sources = {
-        nls.builtins.formatting.stylua,
-        nls.builtins.diagnostics.eslint,
-        nls.builtins.completion.spell,
+        nls.builtins.formatting.stylua.with({ extra_args = { "--indent-type", "Spaces", "--indent-width", "2" } }),
+        nls.builtins.diagnostics.eslint_d,
         nls.builtins.formatting.prettier.with({
           extra_args = { "--single-quote", "false" },
         }),
         nls.builtins.formatting.terraform_fmt,
         nls.builtins.formatting.black,
+        nls.builtins.formatting.goimports,
+        nls.builtins.formatting.gofumpt,
+        nls.builtins.formatting.latexindent.with({
+          extra_args = { "-g", "/dev/null" }, -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
+        }),
+        nls.builtins.code_actions.shellcheck,
+        nls.builtins.code_actions.gitsigns,
+        nls.builtins.formatting.shfmt,
+        nls.builtins.diagnostics.ruff,
+        nls.builtins.formatting.rustfmt,
       },
-      on_attach = function(client)
-        if client.server_capabilities.documentFormattingProvider then
-          -- auto format on save (not asynchronous)
-          local LspFormattingGrp = vim.api.nvim_create_augroup("LspFormattingGrp", { clear = true })
+      on_attach = function(client, bufnr)
+        vim.keymap.set(
+          "n",
+          "<leader>tF",
+          "<cmd>lua require('core.plugins.lsp.utils').toggle_autoformat()<cr>",
+          { desc = "Toggle format on save" }
+        )
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
           vim.api.nvim_create_autocmd("BufWritePre", {
-            command = "lua vim.lsp.buf.format()",
-            group = LspFormattingGrp,
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
           })
         end
       end,
