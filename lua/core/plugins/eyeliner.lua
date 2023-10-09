c3cb64ad5d58f6caa0ad8fe5d6f490b497d2a068
senilio <emil@melt.se>
2023-10-09 21:56:18 +0200

    Eyeliner

---
+++ lua/core/plugins/eyeliner.lua
@@ -0,0 +1,11 @@
+local M = {
+  "jinh0/eyeliner.nvim",
+  config = function()
+    require("eyeliner").setup({
+      highlight_on_key = true, -- show highlights only after keypress
+      dim = false, -- dim all other characters if set to true (recommended!)
+    })
+  end,
+}
+
+return M
