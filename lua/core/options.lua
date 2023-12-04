3ccd47711dee77e01f9d60cb0ea851405f7490be
senilio <git@melt.se>
2023-12-04 09:20:35 +0100

    Add Sort plugin and shortcuts

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -4,52 +4,52 @@ local fn = vim.fn
 
 vim.cmd("set inccommand=split")
 
-o.background = "dark"
-o.mouse = ""
-o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50" -- block in normal and beam cursor in insert mode
-o.updatetime = 100 -- faster completion
-o.timeoutlen = 500 -- time to wait for a mapped sequence to complete (in milliseconds)
-o.ttimeoutlen = 0 -- Time in milliseconds to wait for a key code sequence to complete
-o.backup = false -- creates a backup file
-o.swapfile = true -- enable/disable swap file creation
-o.dir = fn.stdpath("data") .. "/swp" -- swap file directory
-o.undofile = true -- enable/disable undo file creation
-o.undodir = fn.stdpath("data") .. "/undodir" -- set undo directory
-o.history = 500 -- Use the 'history' option to set the number of lines from command mode that are remembered.
 -- o.clipboard = "unnamedplus" -- allows neovim to access the system clipboard
-o.fileencoding = "utf-8" -- the encoding written to a file
+o.background = "dark"
+o.backup = false -- creates a backup file
+o.cmdheight = 0 -- space for displaying messages/commands
+o.completeopt = { "menu", "menuone", "noselect", "noinsert" } -- A comma separated list of options for Insert mode completion
 o.conceallevel = 0 -- so that `` is visible in markdown files
+o.cursorline = true -- highlight the current line
+o.dir = fn.stdpath("data") .. "/swp" -- swap file directory
+o.expandtab = false -- convert tabs to spaces
+o.fileencoding = "utf-8" -- the encoding written to a file
+o.foldenable = false -- disable folding; enable with zi
+o.foldexpr = "nvim_treesitter#foldexpr()"
+o.foldmethod = "expr"
+o.grepprg = "rg --hidden --vimgrep --smart-case --"
+o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50" -- block in normal and beam cursor in insert mode
+o.history = 500 -- Use the 'history' option to set the number of lines from command mode that are remembered.
+o.hlsearch = true -- highlight all matches on previous search pattern
+o.ignorecase = true -- ignore case in search patterns
+o.laststatus = 3
+o.lazyredraw = false -- do not redraw screen while running macros
+o.listchars = "eol:¬,tab:> ,trail:~,extends:>,precedes:<,space:␣"
+o.mouse = ""
 o.number = true
 o.relativenumber = false
-o.cmdheight = 0 -- space for displaying messages/commands
+o.scrolloff = 3 -- Minimal number of screen lines to keep above and below the cursor
+o.shiftwidth = 4 -- the number of spaces inserted for each indentation
+o.shortmess = o.shortmess + "c" -- prevent "pattern not found" messages
 o.showmode = false -- we don't need to see things like -- INSERT -- anymore
 o.showtabline = 2 -- always show tabs
-o.laststatus = 3
+o.sidescrolloff = 5 -- The minimal number of columns to scroll horizontally
 o.smartcase = true -- smart case
 o.smartindent = true -- make indenting smarter again
 o.splitbelow = true -- force all horizontal splits to go below current window
 o.splitright = true -- force all vertical splits to go to the right of current window
-o.expandtab = false -- convert tabs to spaces
-o.shiftwidth = 4 -- the number of spaces inserted for each indentation
+o.swapfile = true -- enable/disable swap file creation
 o.tabstop = 4 -- how many columns a tab counts for
 o.termguicolors = true -- set term gui colors (most terminals support this)
-o.cursorline = true -- highlight the current line
-o.scrolloff = 3 -- Minimal number of screen lines to keep above and below the cursor
-o.sidescrolloff = 5 -- The minimal number of columns to scroll horizontally
-o.hlsearch = true -- highlight all matches on previous search pattern
-o.ignorecase = true -- ignore case in search patterns
-o.foldenable = false -- disable folding; enable with zi
-o.foldmethod = "expr"
-o.foldexpr = "nvim_treesitter#foldexpr()"
-o.listchars = "eol:¬,tab:> ,trail:~,extends:>,precedes:<,space:␣"
-o.shortmess = o.shortmess + "c" -- prevent "pattern not found" messages
-wo.colorcolumn = "99999"
-o.wildmode = "full"
-o.wildmenu = true
-o.lazyredraw = false -- do not redraw screen while running macros
-o.grepprg = "rg --hidden --vimgrep --smart-case --"
-o.completeopt = { "menu", "menuone", "noselect", "noinsert" } -- A comma separated list of options for Insert mode completion
+o.timeoutlen = 500 -- time to wait for a mapped sequence to complete (in milliseconds)
+o.ttimeoutlen = 0 -- Time in milliseconds to wait for a key code sequence to complete
+o.undodir = fn.stdpath("data") .. "/undodir" -- set undo directory
+o.undofile = true -- enable/disable undo file creation
+o.updatetime = 100 -- faster completion
 o.wildignorecase = true -- When set case is ignored when completing file names and directories
+o.wildmenu = true
+o.wildmode = "full"
+wo.colorcolumn = "99999"
 o.wildignore = [[
 .git,.hg,.svn
 *.aux,*.out,*.toc
@@ -61,7 +61,7 @@ o.wildignore = [[
 *.doc,*.pdf,*.cbr,*.cbz
 *.zip,*.tar.gz,*.tar.bz2,*.rar,*.tar.xz,*.kgb
 *.swp,.lock,.DS_Store,._*
-*/tmp/*,*.so,*.swp,*.zip,**/node_modules/**,**/target/**,**.terraform/**"
+*/tmp/*,*.so,*.swp,*.zip,**/node_modules/**,**/target/**,**.terraform/**
 ]]
 vim.cmd("colorscheme ghdark")
 vim.cmd("highlight Normal guibg=#000000")
@@ -73,14 +73,10 @@ vim.cmd("highlight EyelinerPrimary guifg=#7CFC00 gui=underline,bold")
 vim.cmd("highlight EyelinerSecondary guifg=#ffffff gui=bold")
 vim.cmd("highlight MiniIndentscopeSymbol guifg=#444444 gui=NONE")
 
--- Sort ascending
+-- Sort
 vim.cmd([[
-  nnoremap <silent> sa <Cmd>sort<CR>
-  vnoremap <silent> sa <Esc><Cmd>sort<CR>
-]])
-
--- Sort descending
-vim.cmd([[
-  nnoremap <silent> sd <Cmd>sort!<CR>
-  vnoremap <silent> sd <Esc><Cmd>sort!<CR>
+  nnoremap <silent> sa <Cmd>Sort<CR>
+  vnoremap <silent> sa <Esc><Cmd>Sort<CR>
+  nnoremap <silent> sd <Cmd>Sort!<CR>
+  vnoremap <silent> sd v<Esc><Cmd>Sort!<CR>
 ]])
