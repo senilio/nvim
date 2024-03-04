c153171287a36f1a4444d0b27b49bfb0fd822832
senilio <git@melt.se>
2024-03-04 12:51:23 +0100

    Commit none-ls upgrade

--- lua/core/plugins/none-ls.lua
+++ lua/core/plugins/none-ls.lua
@@ -10,22 +10,15 @@ local M = {
     nls.setup({
       sources = {
         nls.builtins.formatting.stylua.with({ extra_args = { "--indent-type", "Spaces", "--indent-width", "2" } }),
-        nls.builtins.diagnostics.eslint_d,
         nls.builtins.formatting.prettier.with({
           extra_args = { "--single-quote", "false" },
         }),
-        nls.builtins.formatting.terraform_fmt,
+        -- nls.builtins.formatting.terraform_fmt,
         nls.builtins.formatting.black,
         nls.builtins.formatting.goimports,
         nls.builtins.formatting.gofumpt,
-        nls.builtins.formatting.latexindent.with({
-          extra_args = { "-g", "/dev/null" }, -- https://github.com/cmhughes/latexindent.pl/releases/tag/V3.9.3
-        }),
-        nls.builtins.code_actions.shellcheck,
         nls.builtins.code_actions.gitsigns,
         nls.builtins.formatting.shfmt,
-        nls.builtins.diagnostics.ruff,
-        nls.builtins.formatting.rustfmt,
         nls.builtins.diagnostics.fish,
         nls.builtins.formatting.fish_indent,
       },
