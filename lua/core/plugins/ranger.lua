c8f3abf7e849131fefacf3a3269eecef8fefc479
senilio <emil@melt.se>
2024-10-26 01:12:26 +0200

    Add ranger for testing

---
+++ lua/core/plugins/ranger.lua
@@ -0,0 +1,21 @@
+return {
+  "kelly-lin/ranger.nvim",
+  config = function()
+    require("ranger-nvim").setup({
+      replace_netrw = true,
+      ui = {
+        border = "single",
+        height = 0.8,
+        width = 0.8,
+        x = 0.5,
+        y = 0.5,
+      },
+    })
+    vim.api.nvim_set_keymap("n", "<leader>ef", "", {
+      noremap = true,
+      callback = function()
+        require("ranger-nvim").open(true)
+      end,
+    })
+  end,
+}
