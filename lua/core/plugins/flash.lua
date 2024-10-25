f0598f550528568e933f16f9d71b19ca4e66b83a
senilio <emil@melt.se>
2024-10-26 00:47:24 +0200

    Flash typo

--- lua/core/plugins/flash.lua
+++ lua/core/plugins/flash.lua
@@ -2,7 +2,7 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  = opts {},
+  opts = {},
   -- stylua: ignore
   keys = {
     { "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
