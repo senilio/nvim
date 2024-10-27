d28c63488638b01bd2fe76ce71c0175cc339c8a2
senilio <emil@melt.se>
2024-10-27 18:59:01 +0100

    Remove unused flash shortcuts

--- lua/core/plugins/flash.lua
+++ lua/core/plugins/flash.lua
@@ -6,7 +6,5 @@ return {
   -- stylua: ignore
   keys = {
     { "ss", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
   },
 }
