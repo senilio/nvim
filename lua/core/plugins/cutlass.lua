efe80b26172dfa154476ddca96f4862bd6f193a5
senilio <git@melt.se>
2024-10-29 10:28:52 +0100

    Add cutlass

---
+++ lua/core/plugins/cutlass.lua
@@ -0,0 +1,14 @@
+return {
+  "gbprod/cutlass.nvim",
+  enabled = true,
+  opts = {
+    cut_key = "x",
+    override_del = true,
+    exclude = {},
+    registers = {
+      select = "_",
+      delete = "_",
+      change = "_",
+    },
+  },
+}
