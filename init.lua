4ff2542b45ea21092b177105a76328f887962ec9
senilio <emil@melt.se>
2024-11-07 20:10:16 +0100

    Update init order

--- init.lua
+++ init.lua
@@ -1,8 +1,8 @@
 -- References to ./lua/
--- Plugin management via Lazy
-require("core.lazy")
 -- Vim mappings, see lua/config/which.lua for more mappings
 require("core.mappings")
+-- Plugin management via Lazy
+require("core.lazy")
 -- All non plugin related (vim) options
 require("core.options")
 -- Vim autocommands/autogroups
--- lua/core/plugins/yazi.lua
+++ lua/core/plugins/yazi.lua
@@ -26,7 +26,7 @@ local M = {
   opts = {
     floating_window_scaling_factor = 0.90,
     open_for_directories = false,
-    yazi_floating_window_border = "double",
+    yazi_floating_window_border = "single",
     hooks = {
       yazi_opened_multiple_files = function(chosen_files, config, state)
         -- Iterate over the selected files
