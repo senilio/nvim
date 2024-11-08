4269181e3f7a98074f7da508a4a633793ab07c8d
senilio <git@melt.se>
2024-11-08 14:07:53 +0100

    Revert order of init

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
 -- References to ./lua/
--- Vim mappings, see lua/config/which.lua for more mappings
-require("core.mappings")
--- Plugin management via Lazy
 require("core.lazy")
--- All non plugin related (vim) options
+require("core.mappings")
 require("core.options")
--- Vim autocommands/autogroups
 require("core.autocmd")
