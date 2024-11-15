71836da9a487f810b088132cc80e09526d7acfc8
senilio <git@melt.se>
2024-11-15 06:49:19 +0100

    Fix mini-pairs

--- lua/core/plugins/mini-pairs.lua
+++ lua/core/plugins/mini-pairs.lua
@@ -23,8 +23,8 @@ return {
         ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
         ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
 
-        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '[^%a\\"].', register = { cr = true } },
-        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a\\].", register = { cr = false } },
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = '[^%a%d\\"].', register = { cr = true } },
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a%d\\].", register = { cr = false } },
         ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^\\].", register = { cr = false } },
       },
     })
