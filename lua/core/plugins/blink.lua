0daf04bcf23b4850618dfb1cb9202efdc357cb65
senilio <emil@melt.se>
2024-11-22 22:04:32 +0100

    Use blink

---
+++ lua/core/plugins/blink.lua
@@ -0,0 +1,36 @@
+local M = {
+  {
+    "saghen/blink.cmp",
+    lazy = false, -- lazy loading handled internally
+    dependencies = "rafamadriz/friendly-snippets",
+    version = "v0.*",
+    opts = {
+      keymap = {
+        preset = "enter",
+      },
+      highlight = {
+        use_nvim_cmp_as_default = true,
+      },
+      nerd_font_variant = "mono",
+      sources = {
+        completion = {
+          enabled_providers = { "lsp", "path", "snippets", "buffer" },
+        },
+      },
+    },
+    opts_extend = { "sources.completion.enabled_providers" },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
+    config = function(_, opts)
+      local lspconfig = require("lspconfig")
+      for server, config in pairs(opts.servers or {}) do
+        config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+        lspconfig[server].setup(config)
+      end
+    end,
+  },
+}
+
+return M
